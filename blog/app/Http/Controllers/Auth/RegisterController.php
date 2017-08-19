3285d831ee14da2aab8ffc7412f20f804011df05
ashucn <ashucn@qq.com>
2017-08-18 17:24:42 -0700

    username unique

--- blog/app/Http/Controllers/Auth/RegisterController.php
+++ blog/app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,7 @@ class RegisterController extends Controller
     {
         // dd($data);
         return Validator::make($data, [
-            'name' => 'required|string|max:10|min:2',
+            'name' => 'required|string|max:10|min:2|unique:users',
             'email' => 'required|string|email|max:28|unique:users',
             'password' => 'required|string|min:6',
         ]);
@@ -66,6 +66,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'status' => 1,
             'password' => bcrypt($data['password']),
         ]);
     }
