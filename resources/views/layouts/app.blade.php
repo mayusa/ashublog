0f51263447745c3d413da68e6185d6bbc372cb62
ashucn <ashucn@qq.com>
2017-08-29 23:42:32 -0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,8 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>{{ config('app.name', 'Ashu Blog') }}</title>
-  @include('includes.csrf-token-js')
+
+  {{-- @include('includes.csrf-token-js') --}}
 
   <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
