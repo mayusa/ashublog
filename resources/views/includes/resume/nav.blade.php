11986e1747aed645fa5a58f30c2e3820b6a652ac
ashucn <ashucn@qq.com>
2017-09-06 17:15:42 -0700

    update

--- resources/views/includes/resume/nav.blade.php
+++ resources/views/includes/resume/nav.blade.php
@@ -1,4 +1,4 @@
-<img src="/resume/images/avatar.jpg" alt="avatar">z
+<img src="/resume/images/avatar.jpg" alt="avatar">
 <h1 class="site-title">@lang('blog.name')</h1>
 
 <nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
